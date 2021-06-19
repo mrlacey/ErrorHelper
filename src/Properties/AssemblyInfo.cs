a550e8f7ff4ffcf5c8f859aa0f8c450731541574
Matt Lacey <matt@mrlacey.co.uk>
2021-06-19 13:34:33 +0100

    v1.8.2 - fix VS2022 dependency (hopefully)

--- src/Properties/AssemblyInfo.cs
+++ src/Properties/AssemblyInfo.cs
@@ -16,5 +16,5 @@ using System.Runtime.InteropServices;
 
 [assembly: ComVisible(false)]
 
-[assembly: AssemblyVersion("1.8.*")]
-[assembly: AssemblyFileVersion("1.8.0.0")]
+[assembly: AssemblyVersion("1.8.2.*")]
+[assembly: AssemblyFileVersion("1.8.2.0")]
