16812dec81512f6d0e9d1e6597eb4a3d6f358363
Matt Lacey <matt@mrlacey.co.uk>
2024-05-02 10:36:23 +0100

    provide notification if the description can't be copied
    For #16

--- src/CommandBase.cs
+++ src/CommandBase.cs
@@ -11,92 +11,92 @@ using Task = System.Threading.Tasks.Task;
 
 namespace ErrorHelper
 {
-    public class CommandBase
-    {
-        public static readonly Guid CommandSet = new Guid("5fea58eb-df34-46e8-ab5d-4708b07cf330");
+	public class CommandBase
+	{
+		public static readonly Guid CommandSet = new Guid("5fea58eb-df34-46e8-ab5d-4708b07cf330");
 
-        private readonly AsyncPackage package;
+		private readonly AsyncPackage package;
 
-        public CommandBase(AsyncPackage package)
-        {
-            this.package = package ?? throw new ArgumentNullException(nameof(package));
-        }
+		public CommandBase(AsyncPackage package)
+		{
+			this.package = package ?? throw new ArgumentNullException(nameof(package));
+		}
 
-        public async Task<string> GetDescriptionAsync()
-        {
-            await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync();
+		public async Task<string> GetDescriptionAsync()
+		{
+			await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync();
 
-            if (await this.package.GetServiceAsync(typeof(SVsErrorList)) is IVsTaskList2 tasks)
-            {
-                tasks.EnumSelectedItems(out IVsEnumTaskItems itemsEnum);
+			if (await this.package.GetServiceAsync(typeof(SVsErrorList)) is IVsTaskList2 tasks)
+			{
+				tasks.EnumSelectedItems(out IVsEnumTaskItems itemsEnum);
 
-                var vsTaskItem = new IVsTaskItem[1];
+				var vsTaskItem = new IVsTaskItem[1];
 
-                if (itemsEnum.Next(1, vsTaskItem, null) == 0)
-                {
-                    vsTaskItem[0].get_Text(out string description);
+				if (itemsEnum.Next(1, vsTaskItem, null) == 0)
+				{
+					vsTaskItem[0].get_Text(out string description);
 
-                    if (!string.IsNullOrWhiteSpace(description))
-                    {
-                        return description;
-                    }
-                }
-            }
+					if (!string.IsNullOrWhiteSpace(description))
+					{
+						return description;
+					}
+				}
+			}
 
-            return string.Empty;
-        }
+			return string.Empty;
+		}
 
-        protected async Task ShowStatusBarMessageAsync(string message)
-        {
-            await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync();
+		protected async Task ShowStatusBarMessageAsync(string message)
+		{
+			await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync();
 
-            try
-            {
-                if (await this.package.GetServiceAsync(typeof(EnvDTE.DTE)) is DTE dte)
-                {
-                    dte.StatusBar.Text = message;
-                }
-            }
-            catch (Exception exc)
-            {
-                System.Diagnostics.Debug.WriteLine(exc);
-            }
-        }
+			try
+			{
+				if (await this.package.GetServiceAsync(typeof(EnvDTE.DTE)) is DTE dte)
+				{
+					dte.StatusBar.Text = message;
+				}
+			}
+			catch (Exception exc)
+			{
+				System.Diagnostics.Debug.WriteLine(exc);
+			}
+		}
 
-        protected string GetSearchUrlBase()
-        {
-            try
-            {
-                var searchEngine = ErrorHelperPackage.Instance?.Options?.SearchEngine;
+		protected string GetSearchUrlBase()
+		{
+			try
+			{
+				var searchEngine = ErrorHelperPackage.Instance?.Options?.SearchEngine;
 
-                switch (searchEngine)
-                {
-                    case SearchEngine.Google:
-                        return "https://www.google.com/search?q=";
+				switch (searchEngine)
+				{
+					case SearchEngine.Google:
+						return "https://www.google.com/search?q=";
 
-                    case SearchEngine.StackOverflow:
-                        return "https://stackoverflow.com/search?q=";
+					case SearchEngine.StackOverflow:
+						return "https://stackoverflow.com/search?q=";
 
-                    case SearchEngine.Bing:
-                        return "https://www.bing.com/search?q=";
+					case SearchEngine.Bing:
+						return "https://www.bing.com/search?q=";
 
-                    case SearchEngine.DuckDuckGo:
-                        return "https://duckduckgo.com/?q=";
+					case SearchEngine.DuckDuckGo:
+						return "https://duckduckgo.com/?q=";
 
-                    case SearchEngine.Custom:
-                        return ErrorHelperPackage.Instance?.Options?.SearchUrl;
+					case SearchEngine.Custom:
+						return ErrorHelperPackage.Instance?.Options?.SearchUrl;
 
-                    case SearchEngine.Ecosia:
-                    default:
-                        break;
-                }
-            }
-            catch (Exception exc)
-            {
-                System.Diagnostics.Debug.WriteLine(exc);
-            }
+					case SearchEngine.Ecosia:
+					default:
+						break;
+				}
+			}
+			catch (Exception exc)
+			{
+				System.Diagnostics.Debug.WriteLine(exc);
+			}
 
-            return "https://www.ecosia.org/search?q=";
-        }
-    }
+			return "https://www.ecosia.org/search?q=";
+		}
+	}
 }
--- src/CopyDescriptionCommand.cs
+++ src/CopyDescriptionCommand.cs
@@ -9,50 +9,55 @@ using Task = System.Threading.Tasks.Task;
 
 namespace ErrorHelper
 {
-    internal sealed class CopyDescriptionCommand : CommandBase
-    {
-        public const int CommandId = 0x0100;
+	internal sealed class CopyDescriptionCommand : CommandBase
+	{
+		public const int CommandId = 0x0100;
 
-        private CopyDescriptionCommand(AsyncPackage package, OleMenuCommandService commandService)
-            : base(package)
-        {
-            commandService = commandService ?? throw new ArgumentNullException(nameof(commandService));
+		private CopyDescriptionCommand(AsyncPackage package, OleMenuCommandService commandService)
+			: base(package)
+		{
+			commandService = commandService ?? throw new ArgumentNullException(nameof(commandService));
 
-            var menuCommandID = new CommandID(CommandSet, CommandId);
-            var menuItem = new MenuCommand(this.Execute, menuCommandID);
-            commandService.AddCommand(menuItem);
-        }
+			var menuCommandID = new CommandID(CommandSet, CommandId);
+			var menuItem = new MenuCommand(this.Execute, menuCommandID);
+			commandService.AddCommand(menuItem);
+		}
 
-        public static CopyDescriptionCommand Instance { get; private set; }
+		public static CopyDescriptionCommand Instance { get; private set; }
 
-        public static async Task InitializeAsync(AsyncPackage package)
-        {
-            await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync(package.DisposalToken);
+		public static async Task InitializeAsync(AsyncPackage package)
+		{
+			await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync(package.DisposalToken);
 
-            OleMenuCommandService commandService = await package.GetServiceAsync(typeof(IMenuCommandService)) as OleMenuCommandService;
-            Instance = new CopyDescriptionCommand(package, commandService);
-        }
+			OleMenuCommandService commandService = await package.GetServiceAsync(typeof(IMenuCommandService)) as OleMenuCommandService;
+			Instance = new CopyDescriptionCommand(package, commandService);
+		}
 
 #pragma warning disable VSTHRD100 // Avoid async void methods
-        private async void Execute(object sender, EventArgs e)
+		private async void Execute(object sender, EventArgs e)
 #pragma warning restore VSTHRD100 // Avoid async void methods
-        {
-            await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync();
+		{
+			await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync();
 
-            try
-            {
-                var desc = await this.GetDescriptionAsync();
+			try
+			{
+				var desc = await this.GetDescriptionAsync();
 
-                if (!string.IsNullOrEmpty(desc))
-                {
-                    System.Windows.Forms.Clipboard.SetText(desc);
-                }
-            }
-            catch (Exception exc)
-            {
-                OutputPane.Instance.WriteLine($"ErrorHelper: {exc}");
-                await this.ShowStatusBarMessageAsync("Unable to copy error description.");
-            }
-        }
-    }
+				if (!string.IsNullOrEmpty(desc))
+				{
+					System.Windows.Forms.Clipboard.SetText(desc);
+				}
+				else
+				{
+					OutputPane.Instance.WriteLine($"Description could not be found.");
+					await this.ShowStatusBarMessageAsync("Unable to copy the description.");
+				}
+			}
+			catch (Exception exc)
+			{
+				OutputPane.Instance.WriteLine($"ErrorHelper: {exc}");
+				await this.ShowStatusBarMessageAsync("Unable to copy error description.");
+			}
+		}
+	}
 }
