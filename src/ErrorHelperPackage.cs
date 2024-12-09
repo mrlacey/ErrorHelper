2931eed8fdcf245aa6bf3d2a5a10a9803faeae50
Matt Lacey <matt@mrlacey.co.uk>
2024-12-09 07:10:06 +0000

    add MSINternal check to analytics

--- src/ErrorHelperPackage.cs
+++ src/ErrorHelperPackage.cs
@@ -75,6 +75,7 @@ namespace ErrorHelper
 					{ "VsixVersion", Vsix.Version },
 					{ "VsVersion", Microsoft.VisualStudio.Telemetry.TelemetryService.DefaultSession?.GetSharedProperty("VS.Core.ExeVersion") },
 					{ "Architecture", RuntimeInformation.ProcessArchitecture.ToString() },
+					{ "MsInternal", Microsoft.VisualStudio.Telemetry.TelemetryService.DefaultSession?.IsUserMicrosoftInternal.ToString() },
 				};
 
 				client.TrackEvent(Vsix.Name, properties);
